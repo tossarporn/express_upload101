37b6ce9dc924dffce2dafdb0a89ef5979461c0d4
tossarporn supanikorn <tossarporn@gmail.com>
2025-02-14 15:11:16 +0700

    add path and fileName is dateTime come in

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 const express = require('express');
 const multer = require('multer');
+
+const storage = multer.diskStorage({
+    destination: function (req,file,cb){
+        cb(null, 'uploads')
+    },
+    filename: function(req,file,cb){
+        // const uniqueSuffix = Date.now() + '-' + Math.round(Math.random());
+        const fileName = `${Date.now()}-${file.originalname}`//originalname อัปโหลดไฟล์ไหนใช้ชื่อไฟล์นั้น
+        cb(null,fileName)
+    }
+})
+
+const upload = multer({storage});
 const cors = require('cors');
 
-const upload = multer({dest: 'uploads/'});
 const app = express();
-
 app.use(cors());
 let port = 8000;
+
+
 
 app.post('/upload',upload.single('test'),(req,res)=>{
     res.json({message: 'hello world'})
