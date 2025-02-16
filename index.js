3868258cebf298ed565566dff3c95ef8c49b1bea
tossarporn supanikorn <tossarporn@gmail.com>
2025-02-16 14:52:16 +0700

    resolve error message from backEnd

--- index.js
+++ index.js
@@ -20,24 +20,26 @@ const storage = multer.diskStorage({
     }
 }) 
 
+
 const upload = multer({
     storage,
     // limits:{//กำหนดขนาด size
     //     fileSize: 1024*1024*2 //แปลงเป็น kg byte * mg byte *2 ขนาไฟล์ไม่ถึง 2 mb //หน่วยเป็น byte
     // },
-    // fileFilter:(req,file,cb) =>{
-    //     if(file.mimetype === 'image/png'){
-    //         //allow
-    //         cb(null,true);//การใส่ true คือ อนุญาต ให้ upload files ได้
-    //     }
-    //     else{
-    //         //not allow
-    //         //การใส่ false คือ ไม่อนุญาต ให้ upload files ได้
-    //         //new Error('not allow other files without image/png') คือให้แสดง ข้อความตอบกลับไป
-    //         cb(new Error('not allow other files without image/png'), false)
-    //     }
-    // }
+    fileFilter:(req,file,cb) =>{
+        if(file.mimetype === 'image/png'){
+            //allow
+            cb(null,true);//การใส่ true คือ อนุญาต ให้ upload files ได้
+        }
+        else{
+            //not allow
+            //การใส่ false คือ ไม่อนุญาต ให้ upload files ได้
+            //new Error('not allow other files without image/png') คือให้แสดง ข้อความตอบกลับไป
+            cb(new Error('not allow other files without image/png'), false)
+        }
+    }
 });
+
 
 const path = require('path');//จัดการไฟล์ใน node.js
 const fs = require('fs');//จัดการไฟล์ใน node.js
@@ -48,20 +50,15 @@ let port = 8000;
 
 
 //ระบุ part,ระบุการเก็บรูปภาพ แบบเดี่ยว หรือ หลายไฟล์ (key ที่รับจาก frontEnd),(req,res)
-app.post('/upload',upload.single('test'),(req,res)=>{//ส่งผ่าน key ที่ชื่อ test
-    
-    res.json({message: 'upload successful'});
-    
-    // //ย้าย middelware
-    // upload.single('test')(req,res,(err)=>{
-    //     if(err){
-    //         console.log('error',err.message);
-    //         return res.status(400).json({message: err.message});
-    //     }
-    //     res.json({message: 'upload successful'});
-
-    //     next();
-    // }) // คือ callback
+app.post('/upload',(req,res)=>{//ส่งผ่าน key ที่ชื่อ test
+    upload.single('test')(req,res,(err)=>{
+        if(err){
+            console.log('error',err.message)
+           res.status(400).json({message: err.message})
+           return res.req.destroy();
+        }
+        res.json({message: 'upload successful'});
+    })
 })
 
 app.listen(port,()=>{
